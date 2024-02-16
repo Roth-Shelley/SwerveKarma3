a69e1b4c7b7649f3ed5ef7df5c84c90a5932a750
ConvolutionalNeuralNetwork <150545879+ConvolutionalNeuralNetwork@users.noreply.github.com>
2024-02-16 16:37:36 +0200

    testing in robot

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -12,6 +12,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+//testing
 
 public class Robot extends TimedRobot {
   public static CTREConfigs ctreConfigs;
